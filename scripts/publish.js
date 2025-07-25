eaa80417e9eac6991fb5fb37fd0b916ef965026b
jsscheller <jsscheller@users.noreply.github.com>
2025-07-25 09:56:42 -0800

    add publish script

---
+++ scripts/publish.js
@@ -0,0 +1,32 @@
+import * as path from "path";
+import * as fs from "fs/promises";
+import { run } from "runish";
+
+const OUT_DIR = path.resolve("./out");
+const { LIVE } = process.env;
+
+async function main() {
+  await fs.rm(OUT_DIR, { force: true, recursive: true });
+  await fs.mkdir(OUT_DIR, { recursive: true });
+
+  for (const filePath of [
+    "package.json",
+    "LICENSE-MIT",
+    "LICENSE-APACHE",
+    "src/index.js",
+  ]) {
+    await fs.cp(filePath, path.join(OUT_DIR, path.basename(filePath)));
+  }
+
+  const otp = process.argv.find((x) => x.startsWith("--otp="));
+  await run(
+    "npm",
+    ["publish", "--access=public", ...(LIVE ? [otp] : ["--dry-run"])],
+    { cwd: OUT_DIR },
+  );
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
